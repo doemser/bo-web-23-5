9edbc4f77ae7eb533d0db017056641dcb851688b
Dominik Hautau <dominikhautau@gmail.com>
2023-11-06 14:57:57 +0100

    backend create session

--- pages/api/jokes/index.js
+++ pages/api/jokes/index.js
@@ -8,4 +8,17 @@ export default async function handler(request, response) {
     const jokes = await Joke.find();
     return response.status(200).json(jokes);
   }
+
+  if (request.method === "POST") {
+    try {
+      const newJoke = request.body;
+      await Joke.create(newJoke);
+      return response
+        .status(200)
+        .json({ status: "Yup! Der Joke ist jetzt drin" });
+    } catch (error) {
+      console.log(error);
+      return response.status(400).json({ error: error.message });
+    }
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,37 @@
 import JokeList from "../components/JokeList";
+import useSWR from "swr";
 
 export default function HomePage() {
-  return <JokeList />;
+  const { mutate } = useSWR("/api/jokes");
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+
+    const response = await fetch("api/jokes", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      mutate();
+    } else {
+      console.log("response nicht ok?");
+    }
+
+    event.target.reset();
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="joke-input">Enter a new joke</label>
+        <input type="text" id="joke-input" name="joke" maxLength={10} />
+        <button type="submit">Submit</button>
+      </form>
+      <JokeList />
+    </>
+  );
 }
